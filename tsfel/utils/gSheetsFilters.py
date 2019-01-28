5be0d5ba9a4778a340d074f1047b9715cfd6f101
dmfolgado <duarte.m.folgado@gmail.com>
2019-01-28 10:21:23 +0000

    Minor fixes.

--- tsfel/utils/gSheetsFilters.py
+++ tsfel/utils/gSheetsFilters.py
@@ -17,7 +17,7 @@ def filter_features(dic, filters):
         feat_shown = [ff for ff in features_all if ff not in feat_hidden]
     if filters['3'] != {}:
         cost_numbers = filters['3']['hiddenValues']
-        cost_hidden = list(np.concatenate([['Constant','Log'] if cn == 1 else ['Squared','Nlog'] if cn == 3 else ['Linear'] if cn == 2 else ['Unknown'] for cn in cost_numbers]))
+        cost_hidden = list(np.concatenate([['Constant','Log'] if int(cn) == 1 else ['Squared','Nlog'] if int(cn) == 3 else ['Linear'] if int(cn) == 2 else ['Unknown'] for cn in cost_numbers]))
         cost_shown = []
         for dk in dic.keys():
             cost_shown += [ff for ff in dic[dk].keys() if dic[dk][ff]['Complexity'] not in cost_hidden]
