c3c2c2241d604620245945ea7b07b5b419666d85
marilia.barandas <marilia.barandas@fraunhofer.pt>
2019-03-25 18:08:47 +0000

    updated version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tsfel",
-    version="0.0.3",
+    version="0.0.4",
     author="Fraunhofer",
     description="Library for time series feature extraction",
     long_description=long_description,
