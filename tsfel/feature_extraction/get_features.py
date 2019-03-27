3aeb118ef5765a125bab3c92b32097051563bec7
marilia.barandas <marilia.barandas@fraunhofer.pt>
2019-03-27 16:16:40 +0000

    added sampling frequency param to extract_features method

--- tsfel/feature_extraction/get_features.py
+++ tsfel/feature_extraction/get_features.py
@@ -15,12 +15,13 @@ def window_spliter(signal, window_size, overlap):
     return [signal[i:i + window_size] for i in range(0, len(signal) - window_size, step)]
 
 
-def extract_features(sig, label, cfg, filename='Features.csv'):
+def extract_features(sig, label, cfg, fs, filename='Features.csv'):
     """
 
     :param sig: list of signal windows
     :param label: label name to be concatenated with feature name
     :param cfg: dictionary with selected features from json file
+    :param fs: sampling frequency
     :param filename: optional parameter; filename for the output file with features values
     :return: features values for each window size
     """
@@ -28,7 +29,7 @@ def extract_features(sig, label, cfg, filename='Features.csv'):
     feature_label = None
     print("*** Feature extraction started ***")
     for wind_idx, wind_sig in enumerate(sig):
-        row_idx, feature_label = feat_extract(cfg, wind_sig, label)
+        row_idx, feature_label = feat_extract(cfg, wind_sig, label, FS=fs)
         feat_val = row_idx if wind_idx == 0 else np.vstack((feat_val, row_idx))
     feat_val = np.array(feat_val)
     d = {str(lab): feat_val[:,idx] for idx, lab in enumerate(feature_label)}
