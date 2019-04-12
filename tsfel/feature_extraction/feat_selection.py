838de6dbf53d40cdf3ada634593b3128ebb86f81
Marilia Barandas <marilia.barandas@fraunhofer.pt>
2019-04-12 16:14:35 +0100

    Improved foward feature selection

--- tsfel/feature_extraction/feat_selection.py
+++ tsfel/feature_extraction/feat_selection.py
@@ -99,13 +99,17 @@ def forward_feature_selection(clf, X, y, groups=None, cv=10, n_jobs=-1, feature_
     if n_jobs == -1:
         n_jobs = multiprocessing.cpu_count()
 
+    # Copy features array
+    all_feat = X.copy()
+    all_feat_names = feature_names.copy()
+
     # While accuracy improves
     while new_acc > acc:
         # Update previous accuracy
         acc = new_acc
 
         # Arguments for cross validation
-        cv_args = [(clf, np.hstack((current_features, feat.T.reshape(-1, 1))), y, None, None, cv) for feat in X.T]
+        cv_args = [(clf, np.hstack((current_features, feat.T.reshape(-1, 1))), y, None, None, cv) for feat in all_feat.T]
 
         # Cross validation of current_features hstack with each feature (multi-core parallel)
         with multiprocessing.Pool(n_jobs) as pool:
@@ -122,12 +126,15 @@ def forward_feature_selection(clf, X, y, groups=None, cv=10, n_jobs=-1, feature_
         new_std = np.std(cv_scores[best_f])
         if new_acc > acc:
             # Add new feature to current_features
-            current_features = np.hstack((current_features, X[:, best_f].reshape(-1, 1)))
+            current_features = np.hstack((current_features, all_feat[:, best_f].reshape(-1, 1)))
             # Add argument of new feature to best_features
-            best_features_args.append(best_f)
+            best_features_args.append(np.where(feature_names == all_feat_names[best_f])[0][0])
             # Print current accuracy and best feature's arguments
             print('Accuracy: ' + str(np.round(new_acc * 100, 2)) + ' +/- ' +
                   str(np.round(new_std * 100, 2)), feature_names[best_features_args])
+            # Remove best feature from all_feat
+            all_feat = np.delete(all_feat, best_f, axis=1)
+            all_feat_names = np.delete(all_feat_names, best_f, axis=0)
 
     # Return the svm's probability attribute to its previous state
     if probability is True:
@@ -138,78 +145,6 @@ def forward_feature_selection(clf, X, y, groups=None, cv=10, n_jobs=-1, feature_
     print('Time elapsed: ' + str(np.round(time.time() - t_start, 1)) + 's\n')
 
     return best_features_args
-
-
-def FSE(X_train, X_test, y_train, y_test, features_descrition, classifier):
-    """ Performs a forward feature selection.
-    Parameters
-    ----------
-    X_train: array-like
-      train set features
-    X_test: array-like
-      test set features
-    y_train: array-like
-      train set labels
-    y_test: array-like
-      test set labels
-    y_test: array-like
-      test set labels
-    features_descrition: list of strings
-      list with extracted features names
-    classifier: object
-      classifier object
-    Returns
-    -------
-    FS_X_train: train set best set of features
-    FS_X_test: test set best set of features
-    FS_lab: name of the best set of features
-    """
-    total_acc, FS_lab, acc_list = [], [], []
-    X_train = np.array(X_train)
-    X_test = np.array(X_test)
-
-    print("*** Feature selection started ***")
-    for feat_idx, feat_name in enumerate(features_descrition):
-        classifier.fit(X_train[:,feat_idx].reshape(-1,1), y_train)
-        y_test_predict = classifier.predict(X_test[:,feat_idx].reshape(-1,1))
-        acc_list.append(accuracy_score(y_test, y_test_predict))
-
-    curr_acc_idx = np.argmax(acc_list)
-    FS_lab.append(features_descrition[curr_acc_idx])
-    last_acc = acc_list[curr_acc_idx]
-    FS_X_train = X_train[:,curr_acc_idx]
-    FS_X_test = X_test[:,curr_acc_idx]
-    total_acc.append(last_acc)
-
-    while 1:
-        acc_list = []
-        for feat_idx, feat_name in enumerate(features_descrition):
-            if feat_name not in FS_lab:
-                curr_train = np.column_stack((FS_X_train, X_train[:, feat_idx]))
-                curr_test = np.column_stack((FS_X_test, X_test[:, feat_idx]))
-                classifier.fit(curr_train, y_train)
-                y_test_predict = classifier.predict(curr_test)
-                acc_list.append(accuracy_score(y_test, y_test_predict))
-            else:
-                acc_list.append(0)
-        curr_acc_idx = np.argmax(acc_list)
-        if last_acc < acc_list[curr_acc_idx]:
-            FS_lab.append(features_descrition[curr_acc_idx])
-            last_acc = acc_list[curr_acc_idx]
-            total_acc.append(last_acc)
-
-            FS_X_train = np.column_stack((FS_X_train, X_train[:, curr_acc_idx]))
-            FS_X_test = np.column_stack((FS_X_test, X_test[:, curr_acc_idx]))
-        else:
-            print("FINAL Features: " + str(FS_lab))
-            print("Number of features", len(FS_lab))
-            print("Acc: ", str(total_acc))
-            print("From ", str(len(X_train[0])), "to ", str(len(FS_lab)))
-
-            break
-    print("*** Feature selection finished ***")
-
-    return np.array(FS_X_train), np.array(FS_X_test), np.array(FS_lab)
 
 
 def correlation_report(df):
