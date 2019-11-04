306d204b3c828a0dfd5051879e8ea52f2b478e37
sara.santos <Deudmu67ra>
2019-11-04 16:14:14 +0000

    Interaction between features.json and Google sheets optimization

--- tsfel/utils/gSheetsFilters.py
+++ tsfel/utils/gSheetsFilters.py
@@ -1,13 +1,18 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import numpy as np
-import ast
 import tsfel
+import ast
 from tsfel.feature_extraction.features_settings import compute_dictionary
 from tsfel.utils.calculate_complexity import compute_complexity
 
-
 def filter_features(dic, filters):
+    """
+    @brief: Filtering features based on feature selection from Google sheet
+    :param dic: dict, dictionary with features
+    :param filters: dict, dictionary with filters from Google sheets
+    :return: dict, filtered dictionary
+    """
     features_all = list(np.concatenate([list(dic[dk].keys()) for dk in sorted(dic.keys())]))
     list_shown, feat_shown = list(dic.keys()), features_all
     cost_shown = features_all
@@ -29,34 +34,52 @@ def filter_features(dic, filters):
 
     return features_filtered
 
-
 def extract_sheet(gSheetName):
+    """
+    @brief: Interaction between features.json and Google sheets.
+    :param gSheetName: string, Google Sheet name
+    :return: dict, dictionary of features
+    """
+    # path to tsfel library
     lib_path = tsfel.__path__
-    FEATURES_JSON = lib_path[0] + '/feature_extraction/features.json'
-    DEFAULT = {'use': 'yes', 'metric': 'euclidean', 'free parameters': '', 'number of features': 1, 'parameters': ''}
-    # TODO use load_user_settings
-    DICTIONARY = compute_dictionary(FEATURES_JSON, DEFAULT)
-    scope = ['https://spreadsheets.google.com/feeds',
-             'https://www.googleapis.com/auth/drive']
-    creds = ServiceAccountCredentials.from_json_keyfile_name(lib_path[0] + '/utils/client_secret.json', scope)
-    client = gspread.authorize(creds)
-    confManager = client.open(gSheetName)
-    sheet = confManager.sheet1
-    metadata = confManager.fetch_sheet_metadata()
-    list_of_features = sheet.col_values(2)[4:]
-    filters = metadata['sheets'][sheet.id]['basicFilter']['criteria']
-    list_filt_features = filter_features(DICTIONARY, filters)
 
-    use_or_not = ['TRUE' if lf in list_filt_features else 'FALSE' for lf in list_of_features]
+    # Access to features.json
+
+    FEATURES_JSON = lib_path[0] + '/feature_extraction/features.json'
+    DEFAULT = {'use': 'yes', 'metric': 'euclidean', 'free parameters': '', 'number of features': 1}
+
+    # Dict of features and parameters
+    DICTIONARY = compute_dictionary(FEATURES_JSON, DEFAULT)
 
     len_stat = len(DICTIONARY['Statistical'].keys())
     len_temp = len(DICTIONARY['Temporal'].keys())
     len_spec = len(DICTIONARY['Spectral'].keys())
 
-    assert len(list_of_features) <= (len_spec + len_stat + len_temp), \
-    "To insert a new feature, please add it to data/features.json with the code in src/utils/features.py"
+    # Access to Google sheet
 
-    # add new feature
+    # Scope and credentials using the content of client_secret.json file
+    scope = ['https://spreadsheets.google.com/feeds',
+             'https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name(lib_path[0] + '/utils/client_secret.json', scope)
+    # create a gspread client authorizing it using those credentials.
+    client = gspread.authorize(creds)
+    # and pass it to the spreadsheet name and getting access to sheet1
+    confManager = client.open(gSheetName)
+    sheet = confManager.sheet1
+    metadata = confManager.fetch_sheet_metadata()
+
+    # Features from gsheets
+    list_of_features = sheet.col_values(2)[4:]
+    list_domain = sheet.col_values(3)[4:]
+    filters = metadata['sheets'][sheet.id]['basicFilter']['criteria']
+    list_filt_features = filter_features(DICTIONARY, filters)
+
+    use_or_not = ['TRUE' if lf in list_filt_features else 'FALSE' for lf in list_of_features]
+
+    assert len(list_of_features) <= (len_spec + len_stat + len_temp), \
+        "To insert a new feature, please add it to data/features.json with the code in src/utils/features.py"
+
+    # adds a new feature in gsheets if it is missing from features.json
     if len(list_of_features) < (len_spec + len_stat + len_temp):
         # new feature was added
         for domain in DICTIONARY.keys():
@@ -66,53 +89,58 @@ def extract_sheet(gSheetName):
                     param = ''
                     if feat_dict['free parameters']:
                         param = str({"nbins": [10], 'r': [1]})
-                    if feat_dict['parameters'] == 'FS':
+                    if feat_dict['fs'] != '':
                         param = str({"fs": 100})
                     curve = feat_dict['Complexity']
-                    curves_all = ['Linear','Log','Square','Nlog','Constant']
-                    complexity = compute_complexity(feat, domain, FEATURES_JSON) if curve not in curves_all else 1 if curve in ['Constant','Log'] else 2 if curve == 'Linear' else 3
+                    curves_all = ['Linear', 'Log', 'Square', 'Nlog', 'Constant']
+                    complexity = compute_complexity(feat, domain,
+                                                    FEATURES_JSON) if curve not in curves_all else 1 if curve in [
+                        'Constant', 'Log'] else 2 if curve == 'Linear' else 3
                     new_feat = ['', feat, domain, complexity, param,
                                 feat_dict['description']]
-                    idx_row = sheet.findall(domain)[-1].row
-                    sheet.insert_row(new_feat, idx_row)
+                    # checks if the gsheets has no features
+                    if sheet.findall(domain) == []:
+                        idx_row = 4
+                    else:
+                        idx_row = sheet.findall(domain)[-1].row
+
+                    # Add new feature at the end of feature domain
+                    sheet.insert_row(new_feat, idx_row + 1)
+                    print(feat + " was added to gsheet.")
+
+        # Update list of features an domain from gsheets
         list_of_features = sheet.col_values(2)[4:]
+        list_domain = sheet.col_values(3)[4:]
+        # Update filtered features from gsheets
         filters = metadata['sheets'][sheet.id]['basicFilter']['criteria']
         list_filt_features = filter_features(DICTIONARY, filters)
         use_or_not = ['TRUE' if lf in list_filt_features else 'FALSE' for lf in list_of_features]
-        
-        len_stat = len(DICTIONARY['Statistical'].keys())
-        len_temp = len(DICTIONARY['Temporal'].keys())
-        len_spec = len(DICTIONARY['Spectral'].keys())
-    
+
     assert 'TRUE' in use_or_not, 'Please select a feature to extract!' + '\n'
 
-    for i in range(len_stat):
-        if use_or_not[i] == 'TRUE':
-            DICTIONARY['Statistical'][list_of_features[i]]['use'] = 'yes'
-            if list_of_features[i] == 'Histogram':
-                val = sheet.cell(i + 5, 5).value
-                DICTIONARY['Statistical'][list_of_features[i]]['free parameters'] = {
-                    'nbins': ast.literal_eval(val)['nbins'], "r": ast.literal_eval(val)['r']}
-        else:
-            DICTIONARY['Statistical'][list_of_features[i]]['use']='no'
+    # Update dict of features with changes from gsheets
+    for ii, feature in enumerate(list_of_features):
+        domain = list_domain[ii]
+        if use_or_not[ii] == 'TRUE':
+            DICTIONARY[domain][feature]['use'] = 'yes'
 
-    for i in range(len_temp):
-        if use_or_not[i+len_stat] == 'TRUE':
-            DICTIONARY['Temporal'][list_of_features[i+len_stat]]['use'] = 'yes'
-        else:
-            DICTIONARY['Temporal'][list_of_features[i+len_stat]]['use']='no'
+            # Check for histogram free parameters: nbins and r
+            if feature == 'Histogram':
+                if sheet.cell(ii + 5, 5).value != '':
+                    val = sheet.cell(ii + 5, 5).value
+                    # update dic of features based on gsheets histogram parameters
+                    DICTIONARY[domain][feature]['free parameters'] = {
+                'nbins': ast.literal_eval(val)['nbins'], "r": ast.literal_eval(val)['r']}
 
-    for i in range(len_spec):
-        if use_or_not[i+len_stat+len_temp] == 'TRUE':
-            val = sheet.cell(i+len_stat+len_temp+5, 5).value
-            if not val:
-                DICTIONARY['Spectral'][list_of_features[i+len_stat+len_temp]]['parameters'] = ''
-            else:
-                DICTIONARY['Spectral'][list_of_features[i+len_stat+len_temp]]['parameters'] = str(ast.literal_eval(val)['fs'])
-                DICTIONARY['Spectral'][list_of_features[i+len_stat+len_temp]]['use'] = 'yes'
+            # Check features that use sampling frequency parameter
+            if DICTIONARY[domain][feature]['fs'] != '':
+                if (sheet.cell(4, 8).value != '') or (type(sheet.cell(4, 8).value) == str):
+                    # update dict of features based on gsheet fs
+                    val = sheet.cell(4, 8).value
+                    DICTIONARY[domain][feature]['fs'] = val
+                    # update fs parameter in gsheet
+                    param = str({"fs": float(val)})
+                    sheet.update_cell(ii + 5, 5, param)
         else:
-            DICTIONARY['Spectral'][list_of_features[i+len_stat+len_temp]]['use']='no'
-
+            DICTIONARY[domain][feature]['use'] = 'no'
     return DICTIONARY
-
-#extract_sheet()
