b754ca601c71ee78b3482bcb5ba64c134198fa21
Letícia Fernandes <leticia.fernandes@fraunhofer.pt>
2019-11-25 15:14:45 +0000

    get_features_by_domain bug fix; removal of get_all_features

--- tsfel/feature_extraction/features_settings.py
+++ tsfel/feature_extraction/features_settings.py
@@ -20,46 +20,35 @@ def load_json(filename):
     return json.load(open(filename))
 
 
-def get_features_by_domain(domain, filename=None):
+def get_features_by_domain(domain=None, filename=None):
     """Creates a dictionary with the features settings by domain.
 
     Parameters
     ----------
     domain : string
         Available domains: "statistical"; "spectral"; "temporal"
+        If domain equals None, then the features settings from all domains are returned.
     filename : str
         Directory of json file. Default: package features.json directory
 
     Returns
     -------
     Dict
-        Dictionary with the features settings by domain
+        Dictionary with the features settings
 
     """
+
+    if domain not in ['statistical', 'temporal', 'spectral', None]:
+        raise SystemExit('No valid domain. Choose: statistical, temporal, spectral or None (for all feature settings).')
 
     if filename is None:
         filename = tsfel.__path__[0]+"/feature_extraction/features.json"
 
     dict_features = load_json(filename)
-    settings = dict_features[domain]
+    if domain is None:
+        return dict_features
+    else:
+        return {domain: dict_features[domain]}
 
-    return settings
-
-
-def get_all_features():
-    """Creates a dictionary with the features settings from all domains.
-
-    Returns
-    -------
-    Dict
-        Dictionary with the features settings from all domains
-
-    """
-
-    settings = {'statistical': get_features_by_domain('statistical')['statistical'],
-                'temporal': get_features_by_domain('temporal')['temporal'],
-                'spectral': get_features_by_domain('spectral')['spectral']}
-
-    return settings
 
 
