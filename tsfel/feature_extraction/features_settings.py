244ca170775c4583887d4f13f4c903b3abd303b7
Sara Santos <sarasantos@Saras-MacBook-Pro.local>
2020-10-14 18:07:05 +0100

    Tagging features by type of input signal. Update on features.json and features_settings.py.

--- tsfel/feature_extraction/features_settings.py
+++ tsfel/feature_extraction/features_settings.py
@@ -40,7 +40,7 @@ def get_features_by_domain(domain=None, json_path=None):
     """
 
     if json_path is None:
-        json_path = tsfel.__path__[0]+"/feature_extraction/features.json"
+        json_path = tsfel.__path__[0] + "/feature_extraction/features.json"
 
         if domain not in ['statistical', 'temporal', 'spectral', None]:
             raise SystemExit(
@@ -51,6 +51,50 @@ def get_features_by_domain(domain=None, json_path=None):
         return dict_features
     else:
         return {domain: dict_features[domain]}
+
+
+def get_features_by_tag(tag=None, json_path=None):
+    """Creates a dictionary with the features settings by tag.
+
+    Parameters
+    ----------
+    tag : string
+        Available tags: "audio"; "inertial", "ecg"; "eeg"; "emg".
+        If tag equals None then, all available features are returned.
+    json_path : string
+        Directory of json file. Default: package features.json directory
+
+    Returns
+    -------
+    Dict
+        Dictionary with the features settings
+
+    """
+    if json_path is None:
+        json_path = tsfel.__path__[0] + "/feature_extraction/features.json"
+
+        if tag not in ["audio", "inertial", "ecg", "eeg", "emg", None]:
+            raise SystemExit(
+                "No valid tag. Choose: audio, inertial, ecg, eeg, emg or None.")
+
+    features_tag = {}
+    dict_features = load_json(json_path)
+    if tag is None:
+        return dict_features
+    else:
+        for domain in dict_features:
+            features_tag[domain] = {}
+            for feat in dict_features[domain]:
+                if dict_features[domain][feat]["use"] == "no":
+                    continue
+                js_tag = dict_features[domain][feat]["tag"]
+                if isinstance(js_tag, list):
+                    if any([tag in js_t for js_t in js_tag]):
+                        features_tag[domain].update({feat: dict_features[domain][feat]})
+                elif js_tag == tag:
+                    features_tag[domain].update({feat: dict_features[domain][feat]})
+        # to remove empty dicts
+        return dict([[d, features_tag[d]] for d in list(features_tag.keys()) if bool(features_tag[d])])
 
 
 def get_number_features(dict_features):
@@ -80,6 +124,6 @@ def get_number_features(dict_features):
                 if isinstance(n_feat_param, int):
                     number_features += n_feat_param
                 else:
-                    number_features += eval("len("+n_feat_param+")")
+                    number_features += eval("len(" + n_feat_param + ")")
 
     return number_features
