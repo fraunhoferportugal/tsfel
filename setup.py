be713dc4cfa036c13332943e65ab258d4560111c
Tony Silva <tony.silva@aicos.fraunhofer.pt>
2022-03-28 09:42:35 +0100

    Separate docs packages.

--- setup.py
+++ setup.py
@@ -1,15 +1,24 @@
 import setuptools
+from pathlib import Path
+
+ROOT = Path(__file__).parent
 
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open('requirements.txt', 'r') as f:
-    install_reqs = [
-        s for s in [
-            line.strip(' \n') for line in f
-        ] if not s.startswith('#') and s != ''
-    ]
+
+def find_requirements(filename):
+    with (ROOT / "requirements" / filename).open() as f:
+        return [
+            s
+            for s in [line.strip(" \n") for line in f]
+            if not s.startswith("#") and s != ""
+        ]
+
+
+install_reqs = find_requirements("requirements.txt")
+docs_require = find_requirements("requirements-docs.txt")
 
 setuptools.setup(
     name="tsfel",
@@ -19,12 +28,17 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/fraunhoferportugal/tsfel/",
-    package_data={'tsfel': ['feature_extraction/features.json', 'utils/client_secret.json']},
+    package_data={
+        "tsfel": ["feature_extraction/features.json", "utils/client_secret.json"]
+    },
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
     ],
-    install_requires=install_reqs
+    install_requires=install_reqs,
+    extras_require={
+        "docs": docs_require,
+    },
 )
